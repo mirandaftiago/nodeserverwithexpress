4c3cbd232e28d38a654f1fe9b2d09d18ff7f257f
Tiago <miranda.f.tiago@gmail.com>
2021-05-28 19:27:10 +0100

    node server with express - added res.send()

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ app.use((req, res, next) => {
   next(); //Allows the request to continue to the next middleware in line
 });
 
-app.use((req, rest, next) => {
+app.use((req, res, next) => {
   console.log('In another middleware');
+  res.send('<h1>Hello from Express!</h1>');
 });
 
 const server = http.createServer(app);
