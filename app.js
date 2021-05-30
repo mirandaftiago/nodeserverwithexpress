9daf55edf658e5065ff58a48d82b231b8b90885e
Tiago <miranda.f.tiago@gmail.com>
2021-05-30 07:26:16 +0100

    node server with express - added 404 page

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const { reset } = require('nodemon');
 const app = express();
 
 const adminRoutes = require('./routes/admin');
-const shopRoutes = require('.routes/shop');
+const shopRoutes = require('./routes/shop');
 
 app.use(express.urlencoded({extended: false}));
 
 app.use(adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not found</h1>');
+});
 
 app.listen(3000);
